f6a9619c12761ca11f1c1ed998fb23864e05c137
thanh tuan <anthanhtuan@gmail.com>
2018-03-18 23:15:49 +0700

    submitted TKDB hompage

--- tuan/app.py
+++ tuan/app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for, request
+import mlab
+from models.phone import Phone
 
+mlab.connect()
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
-
+    phone_list = Phone.objects()
+    return render_template('index.html', phone_list= phone_list)
 
 if __name__ == '__main__':
   app.run(debug=True)
