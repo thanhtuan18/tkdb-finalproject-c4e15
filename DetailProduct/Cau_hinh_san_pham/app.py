bbb95a25b9e119f0b571b61f99a8276cc39af207
bach2o <bachchakmak@gmail.com>
2018-03-30 23:57:37 +0700

    a

--- DetailProduct/Cau_hinh_san_pham/app.py
+++ DetailProduct/Cau_hinh_san_pham/app.py
@@ -1,8 +1,3 @@
-# from flask import Flask, render_template, redirect, url_for, request
-# import mlab
-# from models.phone import Phone
-
-
 from flask import Flask, render_template, redirect, url_for, request, session
 import mlab
 from models.phone import Phone
@@ -12,21 +7,33 @@ import re
 mlab.connect()
 app = Flask(__name__)
 
-app.secret_key = "tuan@123"
+def brandname():
+    phone = Phone.objects()
+    brand_name_list = []
+    brand_name =""
+    for item in phone:
+        if brand_name != item.phone_brand_name:
+            brand_name = item.phone_brand_name
+            brand_name_list.append(brand_name)
+    return(brand_name_list)
 
+
+# app.secret_key = "tuan@123"
+#
 @app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "GET":
         # phone_list = Phone.objects()
         SP_ordered = Average.objects().order_by("-averagePoint") # dau tru tuc la largest to smallest
         top4 = []
+        brand_name_list = brandname()
         for i in SP_ordered[0:4]:
             phone_dict = {}
-            phone_dict["phone"] = Phone.objects.get(id=i.phone)
+            phone_dict["phone"] = Phone.objects.get(id=i.phone.id)
             phone_dict["averagePoint"] = round(i.averagePoint, 1)
             top4.append(phone_dict)
             print(top4)
-        return render_template('index.html', top4=top4)
+        return render_template('index.html', top4=top4, brand_name_list = brand_name_list)
 
     elif request.method == "POST":
         # form = request.form
@@ -41,6 +48,99 @@ def index():
 
         return redirect("/result")
 
+@app.route('/hang-dien-thoai')
+def cac_hang_dien_thoai():
+    brand_name_list = brandname()
+    return render_template('hang-dien-thoai.html', brand_name_list = brand_name_list)
+
+@app.route('/hang-dien-thoai/<brand_name>')
+def chi_tiet_hang_dien_thoai(brand_name):
+    phone = Phone.objects()
+    phone_list = []
+    phone_data = []
+    for item in phone:
+        if item.phone_brand_name == brand_name:
+            name = item.product_name
+            phone_data.append(item)
+    return render_template('dien-thoai-cua-hang.html',phone = phone_data, brand_name = brand_name)
+
+@app.route('/danhgiasanpham/<proid>',methods = ['GET','POST'])
+def evaluate(proid):
+    if request.method == 'GET':
+        phone = Phone.objects.with_id(proid)
+        for evaluated in Average.objects():
+            if evaluated.phone.id == phone.id:
+                n = round(evaluated['averagePoint'])
+                R = round((255 * (5 - n)) / 5)
+                G = round((255 * n) / 5)
+                B = 0
+                return render_template('Detail/product_detail.html',product = phone,red = R, green = G, blue = B, score = n)
+    elif request.method == 'POST':
+        designlist = []
+        screenlist = []
+        funclist = []
+        explist = []
+        camlist = []
+        pinlist = []
+
+        form = request.form
+        phone = Phone.objects.get(id = proid)
+        design = int(form['design'])
+        print(design)
+        screen = int(form['screen'])
+        func = int(form['func'])
+        exp = int(form['exp'])
+        cam = int(form['cam'])
+        pin = int(form['pin'])
+
+        new_eva = Evaluate(phone = phone,
+                           design = design,
+                           screen = screen,
+                           func = func,
+                           exp = exp,
+                           cam = cam,
+                           pin = pin)
+
+
+        new_eva.save()
+
+        totalEva = Evaluate.objects(phone = phone)
+        new_averagePoint = Average.objects.get(phone = phone)
+
+        for object in totalEva:
+            designlist.append(object['design'])
+            screenlist.append(object['screen'])
+            funclist.append(object['func'])
+            explist.append(object['exp'])
+            camlist.append(object['cam'])
+            pinlist.append(object['pin'])
+
+
+        designsum = sum(designlist)
+        avgdesign = designsum / len(designlist)
+        screensum = sum(screenlist)
+        avgscreen = screensum/ len(designlist)
+        funcsum = sum(funclist)
+        avgfunc = funcsum / len(designlist)
+        expsum = sum(explist)
+        avgexp = expsum / len(designlist)
+        camsum = sum(camlist)
+        avgcam = camsum / len(designlist)
+        pinsum = sum(pinlist)
+        avgpin = pinsum / len(designlist)
+
+        total = avgdesign + avgscreen + avgfunc + avgexp + avgcam + avgpin
+
+        average = total / 6
+
+        new_averagePoint.update(set__averagePoint = average)
+
+        n = round(average)
+        R = round((255 * (5 - n)) / 5)
+        G = round((255 * n) / 5)
+        B = 0
+
+        return render_template("Detail/product_detail.html", score = "{0:.1f}".format(average), product = phone,red = R, green = G, blue = B)
 
 @app.route('/result', methods=["GET", "POST"])
 def result():
